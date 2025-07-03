df0dc02fd6bb35e5a62f30a6c3f90cd3afc7f1fa
zarad509 <zarad509@gmail.com>
2025-07-02 19:27:40 -0500

    changes to folder 2

--- 2/index.js
+++ 2/index.js
@@ -1,31 +1,62 @@
-const input = document.getElementById("input")
+// PALINDROME
+const input = document.getElementById("input") //creating const input and getting the line that we want
 
+//logic of reversing the string
+function reverseString(str) { //create a function called reverseString with str as parameter
 
-function reverseString(str) { //create function called reverseString
-// created str as parameter cuz it requires more logic  
-//we're gonna return str as an array
-//we split the value of str by letters hence "" 
-//then reverse the array and join the letters together again as a string
-
-    return str.split("").reverse().join("");
+    return str.split("").reverse().join(""); //split str into array of characters -> reverse the characters -> join back the characters
+    // returning it to the user 
     
 }
 
-// this is the function that will be called when we click the button
+//onclick button
 function check() { 
-    const value =  input.value;
-    const reverse = reverseString(value); //calling the reverseString function and passing the value of input to it
-    // this will reverse the string and store it in the reverse variable
-    // then we can use it to alert the user or do whatever we want with it
+    const value =  input.value; //creating a const called value which is whateber the user inputs
+    const reverse = reverseString(value); //reverse whatever user inputs
 
-    if (value === reverse) {
+
+//alerts 
+    if (value == 0) {
+        alert ("please type something");
+        return; //stops the rest of alerts
+    }
+    if (value === reverse) { 
         alert("yay! It's a palindrome!!");
     } else {
         alert("wrong xd");
     }
 
-    //remove input after checking
+    
+
+//remove input after checking
     input.value = "";
     }
+
+
+// ----------------------------------------
+
+
+//REVERSE YOUR SENTENCE
+const input2 = document.getElementById("input2")
+
+//logic behind reverse the sentence
+function reverseWords(words) {
+    return words.split(" ").reverse().join(" ");
+
+}
+
+//calling onclick button - logic only happens if button is clicked
+function reverse() {
+    const value2 = input2.value;
+    const reverse2 = reverseWords(value2);
+
+//alerts
+    if (value2.includes(" ")) { //if value has a space
+        alert(reverse2);
+    } else {
+        alert ("please separate your words with a space");
+    }
+
+}
 
 
---
+++ 3/index.js
@@ -0,0 +1,23 @@
+const quotes = [
+    "The wound is the place where the Light enters you.",
+    "What you seek is seeking you.",
+    "Try not to resist the changes that come your way. Instead, let life live through you.",
+    "You were born with wings, why prefer to crawl through life?",
+    "Don't grieve. Anything you lose comes round in another form.",
+    "The only lasting beauty is the beauty of the heart.",
+    "The only way to escape the feeling of loneliness is to love yourself.",
+    "The wound is the place where the Light enters you.",
+    "Stop acting so small. You are the universe in ecstatic motion.",
+    "Let yourself be silently drawn by the strange pull of what you really love. It will not lead you astray.",
+    "Be like a tree and let the dead leaves drop.",
+    "Raise your words, not your voice. It is rain that grows flowers, not thunder.",
+    "Happiness is not something ready-made. It comes from your own actions.",
+    "You have to keep breaking your heart until it opens.",
+]
+
+const usedInexes = new Set(); // we're making a new set
+const quoteElement = document.getElementById("quote"); // this is the tag where the quote will be displayed
+
+function generateQuote() {
+    const randomIdx = Math.floor 
+}
